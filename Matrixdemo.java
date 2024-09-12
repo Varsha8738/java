d5debe0320feb3b084eb1f31a6f5dd46f4c95097
Varsha8738 <sreevarshapenta@gmail.com>
2024-09-12 21:02:57 +0530

    Create Matrixdemo.java

---
+++ Matrixdemo.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+public class Matrixdemo{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of the matrix:");
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int arr[][] = new int[r][c];
+        System.out.println("Enter " + ( r * c ) +" elements into the matrix:");
+        for(int i = 0; i<r;i++){
+            for(int j=0;j<c;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("The elements of the matrix \n");
+        for(int i=0; i<r; i++){
+            for(int j=0;j<c;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+        }
+    }
+}

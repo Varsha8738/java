d155a3e9f71a33670af1bf4da5f458f036a533a4
Varsha8738 <sreevarshapenta@gmail.com>
2024-10-26 11:45:38 +0530

    Create Stringdemo.java

---
+++ Stringdemo.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.lang.String;
+public class Stringdemo{
+    public static void main(String args[]){
+        String s1 = "Welcome";
+        char ch[] = {'V','A','R','S','H','A'}
+        String s2 = new String(ch);
+        String s3 = new String("Example");
+        System.out.println("The first string is:" + s1);
+        System.out.println("The second string is:"+s2);
+        System.out.println("The third string is:"+s3);
+    }
+}
